d716988cbef6c0e8ed9e3e437adb8ecab13a6f77
Jayant Chaudhary <jayantchaudhary2002@gmail.com>
2025-03-20 11:50:32 +0530

    Email 5

--- email.js
+++ email.js
@@ -1,6 +1,6 @@
 // Function to validate email address
 const validateEmail = (email) => {
-    return /^abc([._+\-]\w+)?@bridgelabz\.co(\.\w{2})?$/.test(email); // Ensures '@' is present, 'bridgelabz' is mandatory, '.' follows 'bridgelabz', 'co' is mandatory, and optional part supports allowed special characters
+    return /^abc([._+\-]\w+)?@bridgelabz\.co(\.\w{2})?$/.test(email); // Ensures '@' is present, 'bridgelabz' is mandatory, '.' follows 'bridgelabz', 'co' is mandatory, and TLD (2-char country code) is optional
 };
 
 // Example usage of email validation
@@ -9,8 +9,9 @@ console.log(validateEmail("abc.xyz@bridgelabz.co.in")); // true
 console.log(validateEmail("abc_xyz@bridgelabz.co.in")); // true 
 console.log(validateEmail("abc+xyz@bridgelabz.co.in")); // true 
 console.log(validateEmail("abc-xyz@bridgelabz.co.in")); // true 
+console.log(validateEmail("abc@bridgelabz.co.us")); // true
 console.log(validateEmail("abc@bridgelabz.com")); // false 
 console.log(validateEmail("xyz@bridgelabz.co.in")); // false 
-console.log(validateEmail("abc@xyz.co.in")); // false
+console.log(validateEmail("abc@xyz.co.in")); // false 
 console.log(validateEmail("abc@bridge.co.in")); // false 
-console.log(validateEmail("abc@bridgelabz")); // false
+console.log(validateEmail("abc@bridgelabz")); // false 
