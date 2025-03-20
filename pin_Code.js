81dba0798796661f837c1bf8971ce26840dab63e
Jayant Chaudhary <jayantchaudhary2002@gmail.com>
2025-03-20 11:13:51 +0530

    Pin_code_2

--- pin_Code.js
+++ pin_Code.js
@@ -1,6 +1,6 @@
 // Function to validate Indian PIN code
 const validatePinCode = (pinCode) => {
-    return /^\d{6}$/.test(pinCode);
+    return /^[0-9]\d{5}$/.test(pinCode); // Ensures first character is a digit and total length is 6 digits
 };
 
 // Example usage of PIN code validation
@@ -8,3 +8,4 @@ console.log(validatePinCode("400088")); // true
 console.log(validatePinCode("4008"));   // false
 console.log(validatePinCode("4000888")); // false
 console.log(validatePinCode("40A088"));  // false
+console.log(validatePinCode("A400088")); // false
